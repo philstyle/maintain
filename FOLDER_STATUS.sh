5d3b75eabcb39f52e706df69085dd7344545c32a
Drew Ozier <dozier@zipcar.com>
2017-06-04 14:16:31 -0700

    lazy commit

--- FOLDER_STATUS.sh
+++ FOLDER_STATUS.sh
@@ -8,8 +8,16 @@ fi
 
 for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
 
+ #check
+ OUTPUT=`./LAST_DONE.sh ${FOLDER}/${FILE}`
+ EXITVAL=$?
+ #if exitval > 0, color row red
+ if [ ${EXITVAL} -gt "0" ]; then
+  BEGINNING="<tr style=\"background-color:red\"><td><form method=\"post\">"
+ else
+  BEGINNING="<tr><td><form method=\"post\">"
+ fi
  NAME=`echo ${FILE}| awk -F'.' '{print $1}'`
- BEGINNING="<tr><td><form method=\"post\">"
  END="</td><td><pre>"
  NAMES=""
  #HTML NAME
@@ -23,15 +31,9 @@ for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
  fi
  
  printf "%s" "${NAME}"
-# if [ ${#NAME} -lt "8" ]; then
-#  printf "\t"
-# fi
-# if [ ${#NAME} -lt "16" ]; then
-#  printf "\t"
-# fi
- OUTPUT=`./LAST_DONE.sh ${FOLDER}/${FILE}`
- EXITVAL=$?
- #echo $OUTPUT ${EXITVAL}
+ 
+ 
+ 
  printf "\t%s\t%s"  ${OUTPUT} ${EXITVAL}
  printf "</pre></form></td></tr>"
  echo ""
