ec1a439c48e3218e8d26058d44be449e8e44a0be
Drew Ozier <dozier@zipcar.com>
2017-06-04 13:19:17 -0700

    lazy commit

--- b/index.php
+++ b/index.php
@@ -4,27 +4,40 @@
 table {
     border-collapse: collapse;
     border: 1px solid black;
+    height: 100%;
+    width: 100%;
 }
 tr {
  border: 1px solid black;
+}
+td {
+ text-align: center;
 }
 </style>
 </head>
 <body>
 
-
 <?php
-if (isset($_POST['button']))
+if (isset($_POST['BUTTON']))
     {
-         echo "poop";
+         //shell_exec("echo \"\"");
+         echo "<pre>";
+         $pressed = explode("-",$_POST['BUTTON']);
+         print_r(explode("-",$_POST['BUTTON']));
+         echo "</pre>";
+         shell_exec("UPDATE.sh ${pressed[0]}.mt ${pressed[1]} 2>&1 >> /dev/null &");
+         //echo "<meta http-equiv='refresh' content='0'>";
     }
+    
 
-$DIR=getcwd();    
+$DIR=getcwd();
 $output = shell_exec("pushd .. 2>&1 > /dev/null && ./FOLDER_STATUS.sh $DIR| sort -rk8 && popd 2>&1 >/dev/null");
+
 echo "<table>";
 echo "${output}";
 echo "</table>"
 ?>
+
 
 
 </body>
