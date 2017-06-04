02b097139e5407e624fe745711f52425b8b79990
Drew Ozier <dozier@zipcar.com>
2017-06-04 00:11:43 -0700

    lazy commit

--- FOLDER_STATUS.sh
+++ FOLDER_STATUS.sh
@@ -10,7 +10,7 @@ for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
 
  NAME=`echo ${FILE}| awk -F'.' '{print $1}'`
  #HTML NAME
- NAME="<p><form method=\"post\"><button name=\"button\">${NAME}</button><pre>"
+ NAME="<div><form method=\"post\"><button name=\"button\">${NAME}</button><pre>"
  printf "%s" "${NAME}"
  if [ ${#NAME} -lt "8" ]; then
   printf "\t"
@@ -22,6 +22,6 @@ for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
  EXITVAL=$?
  #echo $OUTPUT ${EXITVAL}
  printf "\t%s\t%s"  ${OUTPUT} ${EXITVAL}
- printf "</pre></form></p>"
+ printf "</pre></form></div>"
  echo ""
 done
