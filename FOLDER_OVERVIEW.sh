c30a6887e76228473c31b0e52abc11454ec514dd
ACK <drew.ozier@gmail.com>
2018-09-20 00:21:32 -0700

    adding overview view

---
+++ FOLDER_OVERVIEW.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#list status of things in folder
+FOLDER=$1
+
+if [ "${FOLDER}x" = "x" ]; then
+ FOLDER="."
+fi
+COLUMNS=4
+COUNT=0
+for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
+
+ #check
+ OUTPUT=`./LAST_DONE.sh ${FOLDER}/${FILE}`
+ EXITVAL=$?
+
+ #if exitval > 0, color row red
+ if [ ${EXITVAL} -gt "0" ]; then
+  BEGINNING="<td style=\"background-color:#dd3b3b;vertical-align:middle\">"
+ else
+  BEGINNING="<td style=\"background-color:#4CAF50;vertical-align:middle\">"
+ fi
+ 
+ NAME=`echo ${FILE}| awk -F'.' '{print $1}' | sed 's/_/ /g'`
+ BUTTON_NAME=`echo ${FILE}| awk -F'.' '{print $1}'`
+ END="</td>"
+ NAMES=""
+ #HTML NAME
+ VAL=1
+ NAME=${BEGINNING}"<button class=\"button${VAL}\" name=\"BUTTON\" value=\"${BUTTON_NAME}\">${NAME}</button>"${END}
+  
+
+ printf "%s" "${NAME}"
+
+ if [ $COUNT -eq $COLUMNS ]; then
+  echo "</tr><tr>"
+  COUNT=0
+ else
+  COUNT=$(( COUNT + 1 ))
+ fi
+
+done
---
+++ d/overview.php
@@ -0,0 +1,101 @@
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    border: 1px solid black;
+    height: 100%;
+    width: 100%;
+}
+.button1 {
+    background-color: #4ca3af;
+    border-radius: 6%;
+    border: none;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 3vmin;
+    cursor: default;
+    width: 18vmin;
+    height: 18vmin;
+}
+.button2 {
+    background-color: #608be0;
+    border-radius: 6%;
+    border: none;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 3vmin;
+    cursor: pointer;
+    width: 16vmin;
+    height: 14vmin;
+}
+.button3 {
+    background-color: #404b90;
+    border-radius: 6%;
+    border: none;
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 2vmin;
+    cursor: pointer;
+    width: 14vmin;
+    height: 8vmin;
+}
+
+.button1:hover {
+    background-color: #4CAF50; /* Green */
+    color: white;
+}
+.button2:hover {
+    background-color: #4CAF50; /* Green */
+    color: white;
+}
+.button3:hover {
+    background-color: #4CAF50; /* Green */
+    color: white;
+}
+
+tr {
+ border: 1px solid black;
+}
+td {
+ text-align: center;
+ font-size: 3vmin;
+ font-weight: bold;
+}
+</style>
+</head>
+<body>
+
+<?php
+if (isset($_POST['BUTTON']))
+    {
+         //shell_exec("echo \"\"");
+         //echo "<pre>";
+         $pressed = explode("-",$_POST['BUTTON']);
+         //print_r(explode("-",$_POST['BUTTON']));
+         //echo "</pre>";
+         $DIR=getcwd();
+         shell_exec("pushd .. 2>&1 > /dev/null && UPDATE.sh ${DIR}/${pressed[0]}.mt} 2>&1 >> /dev/null && popd");
+         echo "<meta http-equiv='refresh' content='0'>";
+    }
+    
+$DIR=getcwd();
+$output = shell_exec("pushd .. 2>&1 > /dev/null && FOLDER_OVERVIEW.sh ${DIR} | sort -rk16 && popd");
+header("Refresh: 30;");
+echo "<table>";
+echo "${output}";
+echo "</table>";
+
+
+?>
+
+
+
+</body>
+</html>
