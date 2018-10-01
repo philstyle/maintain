5a063d24609513602a7263f941f5e09bc83effda
ACK <drew.ozier@gmail.com>
2018-10-01 13:18:14 -0700

    adding initial split for two people. still need workign buttons

---
+++ bd.php
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+<style>
+iframe {
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    height: 30%;
+}
+</style>
+<iframe src="b/overview.php" style="height: 100%; width: 50%"></iframe><iframe src="d/overview.php" style="height: 100%; width: 50%"></iframe>
+</body>
+</html>
