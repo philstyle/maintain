7c8e39ec87269a08eb692d4c9d8cfab13791fa94
ACK <drew.ozier@gmail.com>
2019-05-06 14:04:15 -0700

    change to get points?

--- get_points.sh
+++ get_points.sh
@@ -10,12 +10,35 @@ echo $TASKFILE -- $PERSON
 
 
 
-FREQ=`head -n1 ${THING} | awk '{print $1}'`
-PER=`head -n1 ${THING} | awk '{print $2}'`
-POINTS=`head -n1 ${THING} | awk '{print $3}'`
+FREQ=`head -n1 ${TASKFILE} | awk '{print $1}'`
+PER=`head -n1 ${TASKFILE} | awk '{print $2}'`
+POINTS=`head -n1 ${TASKFILE} | awk '{print $3}'`
 
+
+
+
+case $PER in
+day)
+  SECRET=24
+  ;;
+week)
+  SECRET=7
+  ;;
+month)
+  SECRET=30
+  ;;
+year)
+  SECRET=365
+  ;;
+*)
+  SECRET=7
+  ;;
+esac
+
+MAGIC_NUMBER=$((SECRET / FREQ))
 
 if [ "${PERSON}x" = "x" ]; then
  #empty person, just sum
  echo NOPEEPS 
 fi
+ echo $FREQ $PER $POINTS $SECRET $MAGIC_NUMBER
