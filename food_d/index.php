c9b0401de930e2097c66e9f764bdeba5ca6f37b3
dozier <drew.ozier@gmail.com>
2022-02-14 18:59:47 -0700

    smallll

--- food_d/index.php
+++ food_d/index.php
@@ -10,7 +10,7 @@
     text-align: center;
     text-decoration: none;
     display: inline-block;
-    font-size: 4vmin;
+    font-size: 1vmin;
     font-weight: bold;
     cursor: pointer;
     width: 8em;
