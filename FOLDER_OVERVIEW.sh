834e776bce02ad1b34320d336095d6baaa518ee9
dozier <drew.ozier@gmail.com>
2022-02-14 20:12:31 -0700

    adding form for buttons in overview

--- FOLDER_OVERVIEW.sh
+++ FOLDER_OVERVIEW.sh
@@ -14,14 +14,14 @@ for FILE in `ls -1 ${FOLDER} | grep ".mt$"`; do
 
  #if exitval > 0, color row red
  if [ ${EXITVAL} -gt "0" ]; then
-  BEGINNING="<td style=\"background-color:#dd3b3b;vertical-align:middle\">"
+  BEGINNING="<td style=\"background-color:#dd3b3b;vertical-align:middle\"><form style=\"padding-top:4%\" method=\"post\">"
  else
-  BEGINNING="<td style=\"background-color:#4CAF50;vertical-align:middle\">"
+  BEGINNING="<td style=\"background-color:#4CAF50;vertical-align:middle\"><form style=\"padding-top:4%\" method=\"post\">"
  fi
  
  NAME=`echo ${FILE}| awk -F'.' '{print $1}' | sed 's/_/ /g'`
  BUTTON_NAME=`echo ${FILE}| awk -F'.' '{print $1}'`
- END="</td>"
+ END="</form></td>"
  NAMES=""
  #HTML NAME
  VAL=1
